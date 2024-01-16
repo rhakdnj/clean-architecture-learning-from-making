f1d2439f3a8a07f0bfc7b6b97aa4fe0d07565722
rhakdnj <chungjm0711@gmail.com>
2024-01-17 01:31:44 +0900

    fix: ESLint: All password signatures should be adjacent

--- src/user/domain/entity/user.entity.ts
+++ src/user/domain/entity/user.entity.ts
@@ -18,6 +18,10 @@ export class UserEntity extends Entity<UserProps> {
     this.name = value
   }
 
+  get name(): string {
+    return this.props.name
+  }
+
   private set name(value: string) {
     this.props.name = value
   }
@@ -26,20 +30,16 @@ export class UserEntity extends Entity<UserProps> {
     this.password = value
   }
 
+  get password(): string {
+    return this.props.password
+  }
+
   private set password(value: string) {
     this.props.password = value
   }
 
-  get name(): string {
-    return this.props.name
-  }
-
   get email(): string {
     return this.props.email
-  }
-
-  get password(): string {
-    return this.props.password
   }
 
   get createdAt(): LocalDateTime {
