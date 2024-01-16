46ad9e0ed4bccbba41b5e9196d849e66e01308f1
rhakdnj <chungjm0711@gmail.com>
2024-01-17 00:44:34 +0900

    feat: UserEntity - constructor unit tests

--- src/user/domain/entity/user.entity.ts
+++ src/user/domain/entity/user.entity.ts
@@ -1,1 +1,14 @@
-export class UserEntity {}
+import { LocalDateTime } from '@js-joda/core'
+
+export type UserProps = {
+  name: string
+  email: string
+  password: string
+  createdAt?: LocalDateTime
+}
+
+export class UserEntity {
+  constructor(public readonly props: UserProps) {
+    this.props.createdAt = this.props.createdAt ?? LocalDateTime.now()
+  }
+}
