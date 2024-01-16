fe144383a6338e94d806368be632d321f5510c8e
rhakdnj <chungjm0711@gmail.com>
2024-01-17 01:12:05 +0900

    feat: Update methods - unit tests

--- src/user/domain/entity/__test__/unit/user.entity.spec.ts
+++ src/user/domain/entity/__test__/unit/user.entity.spec.ts
@@ -1,4 +1,3 @@
-import { faker } from '@faker-js/faker'
 import { UserEntity, UserProps } from '@/user/domain/entity/user.entity'
 import { LocalDateTime } from '@js-joda/core'
 import { UserDataBuilder } from '@/user/domain/testing/helper/user-data-builder'
@@ -25,6 +24,12 @@ describe('User entity unit tests', () => {
     expect(typeof sut.props.name).toBe('string')
   })
 
+  it('Setter of name field', () => {
+    sut['name'] = 'other name'
+    expect(sut.props.name).toEqual('other name')
+    expect(typeof sut.props.name).toBe('string')
+  })
+
   it('Getter of email field', () => {
     expect(sut.props.email).toBeDefined()
     expect(sut.props.email).toEqual(props.email)
@@ -37,8 +42,24 @@ describe('User entity unit tests', () => {
     expect(typeof sut.props.password).toBe('string')
   })
 
+  it('Setter of password field', () => {
+    sut['password'] = 'other password'
+    expect(sut.props.password).toEqual('other password')
+    expect(typeof sut.props.password).toBe('string')
+  })
+
   it('Getter of password field', () => {
     expect(sut.props.createdAt).toBeDefined()
     expect(sut.props.createdAt).toBeInstanceOf(LocalDateTime)
+  })
+
+  it('Should update a user', () => {
+    sut.updateName('other name')
+    expect(sut.props.name).toEqual('other name')
+  })
+
+  it('Should update the password field', () => {
+    sut.updatePassword('other password')
+    expect(sut.props.password).toEqual('other password')
   })
 })
