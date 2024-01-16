ef92d8f7b982ffedfa44b56dea369499d26548c7
rhakdnj <chungjm0711@gmail.com>
2024-01-17 00:51:40 +0900

    feat: UserEntity - getters methods unit tests

--- src/user/domain/entity/user.entity.ts
+++ src/user/domain/entity/user.entity.ts
@@ -11,4 +11,20 @@ export class UserEntity {
   constructor(public readonly props: UserProps) {
     this.props.createdAt = this.props.createdAt ?? LocalDateTime.now()
   }
+
+  get name(): string {
+    return this.props.name
+  }
+
+  get email(): string {
+    return this.props.email
+  }
+
+  get password(): string {
+    return this.props.password
+  }
+
+  get createdAt(): LocalDateTime {
+    return this.props.createdAt
+  }
 }
