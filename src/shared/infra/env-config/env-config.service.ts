b0af572934e1c59a163684b082bd49fc434b2fb8
rhakdnj <chungjm0711@gmail.com>
2024-01-16 23:02:57 +0900

    feat: env config module

---
+++ src/shared/infra/env-config/env-config.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common'
+import { EnvConfig } from '@/shared/infra/env-config/env-config.interface'
+import { ConfigService } from '@nestjs/config'
+
+@Injectable()
+export class EnvConfigService implements EnvConfig {
+  constructor(private readonly configService: ConfigService) {}
+  getAppPort(): number {
+    return Number(this.configService.get<number>('PORT'))
+  }
+
+  getNodeEnv(): string {
+    return this.configService.get<string>('NODE_ENV')
+  }
+}
