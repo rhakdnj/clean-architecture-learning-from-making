5b60bd0606b306a89ee80f4c32ecdb33438e8bb9
rhakdnj <chungjm0711@gmail.com>
2024-01-17 23:53:14 +0900

    feat: Integration tests - validate createdAt field on create user

--- src/user/domain/validator/__test__/integration/user.entity.int-spec.ts
+++ src/user/domain/validator/__test__/integration/user.entity.int-spec.ts
@@ -1,6 +1,7 @@
 import { UserEntity, UserProps } from '@/user/domain/entity/user.entity'
 import { UserDataBuilder } from '@/user/domain/testing/helper/user-data-builder'
 import { EntityValidationError } from '@/shared/domain/error/validation-error'
+import { LocalDate } from '@js-joda/core'
 
 describe('UserEntity integration tests', () => {
   describe('Constructor method', () => {
@@ -78,6 +79,26 @@ describe('UserEntity integration tests', () => {
       props = {
         ...UserDataBuilder({}),
         password: 'a'.repeat(101),
+      }
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+    })
+
+    it('Should throw an error when creating a user with invalid createdAt', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        createdAt: '2023' as any,
+      }
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+      props = {
+        ...UserDataBuilder({}),
+        createdAt: 10 as any,
+      }
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+      props = {
+        ...UserDataBuilder({}),
+        createdAt: LocalDate.now() as any,
       }
       expect(() => new UserEntity(props)).toThrow(EntityValidationError)
     })
