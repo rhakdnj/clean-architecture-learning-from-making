4c4e89a9fdf3dbf0517ae8ff6d9ccfd0727b05ed
rhakdnj <chungjm0711@gmail.com>
2024-01-23 00:00:46 +0900

    feat: Unit tests SearchResult class

--- src/shared/domain/repository/__test__/unit/searchable-repository.interface.spec.ts
+++ src/shared/domain/repository/__test__/unit/searchable-repository.interface.spec.ts
@@ -1,4 +1,5 @@
 import { SearchParams } from '@/shared/domain/repository/SearchParams'
+import { SearchResult } from '@/shared/domain/repository/SearchResult'
 
 describe('Searchable Repository unit tests', () => {
   describe('SearchParams tests', () => {
@@ -133,6 +134,72 @@ describe('Searchable Repository unit tests', () => {
       params.forEach(i => {
         expect(new SearchParams({ filter: i.filter }).filter).toBe(i.expected)
       })
+    })
+  })
+
+  describe('SearchResult tests', () => {
+    it('constructor props', () => {
+      let sut = new SearchResult({
+        items: ['test1', 'test2', 'test3', 'test4'] as any,
+        total: 4,
+        currentPage: 1,
+        perPage: 2,
+        sort: null,
+        sortDir: null,
+        filter: null,
+      })
+      expect(sut.toJSON()).toStrictEqual({
+        items: ['test1', 'test2', 'test3', 'test4'] as any,
+        total: 4,
+        currentPage: 1,
+        perPage: 2,
+        lastPage: 2,
+        sort: null,
+        sortDir: null,
+        filter: null,
+      })
+
+      sut = new SearchResult({
+        items: ['test1', 'test2', 'test3', 'test4'] as any,
+        total: 4,
+        currentPage: 1,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'test',
+      })
+      expect(sut.toJSON()).toStrictEqual({
+        items: ['test1', 'test2', 'test3', 'test4'] as any,
+        total: 4,
+        currentPage: 1,
+        perPage: 2,
+        lastPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'test',
+      })
+
+      sut = new SearchResult({
+        items: ['test1', 'test2', 'test3', 'test4'] as any,
+        total: 4,
+        currentPage: 1,
+        perPage: 10,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'test',
+      })
+      expect(sut.lastPage).toBe(1)
+
+      sut = new SearchResult({
+        items: ['test1', 'test2', 'test3', 'test4'] as any,
+        total: 54,
+        currentPage: 1,
+        perPage: 10,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'test',
+      })
+      expect(sut.lastPage).toBe(6)
     })
   })
 })
--- src/shared/domain/repository/searchable-repository.interface.ts
+++ src/shared/domain/repository/searchable-repository.interface.ts
@@ -1,10 +1,13 @@
 import { Entity } from '@/shared/domain/entity/entity'
 import { RepositoryInterface } from '@/shared/domain/repository/repository.interface'
+import { SearchParams } from '@/shared/domain/repository/SearchParams'
+import { SearchResult } from '@/shared/domain/repository/SearchResult'
 
 export interface SearchableRepositoryInterface<
   E extends Entity,
-  SearchInput,
-  SearchOutput,
+  Filter = string,
+  SearchInput = SearchParams,
+  SearchOutput = SearchResult<E, Filter>,
 > extends RepositoryInterface<E> {
   search(props: SearchInput): Promise<SearchOutput>
 }
